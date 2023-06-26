9492a0860eac3c7c40f4ea4c9b0586ea00e16b5e
aquamariney <41199176+bcschutter@users.noreply.github.com>
2023-06-25 19:22:58 -0700

    AudioManager
    - added audiomanager
    - imported audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> m_AudioClips = new List<AudioClip>();
+
+    AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        _audioSource= GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic(int num)
+    {
+        StopMusic();
+
+        _audioSource.clip = m_AudioClips[num];
+
+        _audioSource.Play();
+        _audioSource.loop = true;
+
+        //ProcessClips();
+    }
+
+    void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+
+    // Add clip processing if time to get the looping one going
+    //void ProcessClips()
+    //{
+    //    _audioSource.clip.name = "main-menu-intro";
+
+
+    //}
+}
